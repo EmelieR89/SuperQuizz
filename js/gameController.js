bccbd3fdbce36012b5798e4455b705edefaba464
Claudia Aranda <claudia_15_@hotmail.com>
2020-01-16 12:36:45 +0100

    higheror lower

--- js/gameController.js
+++ js/gameController.js
@@ -1,3 +1,11 @@
 function random(){
     randomize = parseInt(Math.random() * 100);
 }
+
+function higerOrLower() {
+  if (number < randomize) {
+    document.getElementById("higher").innerHTML += "Higher";
+  } else {
+    document.getElementById("lower").innerHTML += "Lower";
+  }
+}
