2af90ffbe16c2c59aac8521b6cabc36a579df651
Claudia Aranda <claudia_15_@hotmail.com>
2020-01-21 15:56:04 +0100

    start page

--- js/StartPageController.js
+++ js/StartPageController.js
@@ -1,37 +1,33 @@
 //code for start page
 class StartPageController{
-    constructor(){
-       // this.selectPageStart = new SelectPage()
-       // this.startGameButton = document.getElementById('gamePageButton')
-       // this.startGameButton.addEventListener('click', this.goToPage.bind(this))
-        
+    constructor(game){
+        this.game = game
         this.rulesT = document.getElementById('readRulesButton')
         this.rulesT.addEventListener('click', this.rulesToggle.bind(this))
+        this.highScoreButton = document.getElementById('highScoreButton')
+        this.highScoreButton.addEventListener('click', this.highScoreToggle.bind(this))
     }
 
-    goToPage(){
-        this.selectPageStart.showPage('game-setup-container')
-    //     this.playerNameInputEl = document.getElementById('startNameInput')
-    //     this.playerNameInputEl.addEventListener('keyup', this.printPlayerName.bind(this))
-    //     this.humanPlayerName = ""
-    
 
-    // printPlayerName(){
-    //     this.humanPlayerName = this.playerNameInputEl.value
-    //     document.querySelector('.start-container').append(this.humanPlayerName)
-    // }
-
-    // getHumanPlayerName(){
-    //     return this.humanPlayerName
-    }
-    
     rulesToggle(){
         let rules = document.getElementById('showRules')
+        console.log(rules.style.display)
         if (rules.style.display === 'none') {
             rules.style.display = 'block'
         } else{
             rules.style.display = 'none'
         }
        
+    }
+
+    highScoreToggle(){
+        this.game.showPage('start-container')
+     
+        if (this.game.getCurrentGameState() === 'start-container') {
+            this.game.showPage('high-score-container')
+        } else{
+            this.game.showPage('start-container')
+        }
+        const highscoreElem = document.querySelector("#highscore-list")
     }
 }
