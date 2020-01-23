02b1d155dc6769a5de5615afdfe08de7d79fabfd
alexbryw <alexbryw@gmail.com>
2020-01-23 13:09:18 +0100

    Added first functions in PlayerManager

---
+++ js/PlayerManager.js
@@ -0,0 +1,23 @@
+class PlayerManager{
+    constructor() {
+        this.currentPlayer
+    }
+
+    createLocalStorage(){
+        //if localStorage does not exist, then create one.
+    }
+
+    findPlayer(playerName){
+        //return player or "not found"
+    }
+
+    createPlayer(){
+        // new player
+    }
+
+    getCurrentPlayer(){
+        //return this.currentPlayer
+    }
+
+
+}
